b03d1ed66f8532ef6c389124984ab55eadd59de2
Leeladhar <bhatshrinidhi10@gmail.com>
2023-02-13 17:53:57 +0530

    hjugsfdhj

---
+++ Dynamic_input.java
@@ -0,0 +1,15 @@
+//dynamic input programm
+
+import java.util.Scanner;
+class Dynamic_input
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the integer value");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+System.out.println("Value is"+" "+a*b*c);
+}
+}
