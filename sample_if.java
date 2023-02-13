82b4b8517f882ca8bce01a553442d7617ed7dc57
Leeladhar <bhatshrinidhi10@gmail.com>
2023-02-13 17:47:43 +0530

    haha

---
+++ sample_if.java
@@ -0,0 +1,17 @@
+class sample_if
+{
+public static void main(String[] args)
+{
+//String S = "Hello";
+double S = 31.5;
+if (S <= 31.2)
+{
+System.out.println("Pass");
+
+}
+else 
+{
+System.out.println("fail");
+}
+}
+}
